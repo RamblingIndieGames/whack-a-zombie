c3ac009408da88ffcf4f63fa5af88f9cc51afcaf
Richard Marks <ccpsceo@gmail.com>
2022-10-01 11:36:20 -0500

    added starting point files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// Imports
+// Constant Declarations
+// Class Types
+// Instance Creation Functions
+// Libraries
+// Global State
+// Global Functions
+// Entry Point Function
+async function main() {
+  console.log("Whack A Zombie - Starting");
+}
+// Entry Point Call
+main().catch((err) => {
+  console.error(err.message);
+  alert(err.message);
+});
