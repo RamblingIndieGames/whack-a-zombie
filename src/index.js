71617aba65fa4e1fbc3f0027d0511011f1f94f12
Richard Marks <ccpsceo@gmail.com>
2022-10-03 19:32:24 -0500

    added createRect instance creation function

--- src/index.js
+++ src/index.js
@@ -20,9 +20,24 @@ const Rect = {
   },
 };
 // Instance Creation Functions
+function createRect(
+  { x, y, width, height } = { x: 0, y: 0, width: 0, height: 0 },
+) {
+  assert(typeof x !== "number", "x must be a Number");
+  assert(typeof y !== "number", "y must be a Number");
+  assert(typeof width !== "number", "width must be a Number");
+  assert(typeof height !== "number", "height must be a Number");
+  const instance = Rect.create(x, y, width, height);
+  return instance;
+}
 // Libraries
 // Global State
 // Global Functions
+function assert(condition, message) {
+  if (condition) {
+    throw new Error(message);
+  }
+}
 // Entry Point Function
 async function main() {
   console.log("Whack A Zombie - Starting");
